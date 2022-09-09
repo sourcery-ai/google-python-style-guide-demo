cb8009c81f06e17cdc3a7ab14aaee3df32197a60
Ruan Comelli <ruancomelli@gmail.com>
2022-09-09 15:20:00 -0300

    docs: add explanation for global variable rules

---
+++ examples/individual_rules/global_variable_rules.py
@@ -0,0 +1,6 @@
+# ----------------------------------------------------------------------------------
+# [2.5 Global Variable Rules](https://docs.sourcery.ai/Reference/Custom-Rules/gpsg/#25-global-variable-rules)
+# ----------------------------------------------------------------------------------
+#
+# This example file contains pieces of code that either comply with or violate the
+# global variable rule `avoid-global-variables`.
