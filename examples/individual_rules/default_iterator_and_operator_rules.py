7cef32bbb6b0323daa3ebbf92d23417756ccfd82
Ruan Comelli <ruancomelli@gmail.com>
2022-09-09 15:24:23 -0300

    docs: add explanation for default iterator and operator rules

---
+++ examples/individual_rules/default_iterator_and_operator_rules.py
@@ -0,0 +1,6 @@
+# ----------------------------------------------------------------------------------
+# [2.8 Default Iterator and Operator Rules](https://docs.sourcery.ai/Reference/Custom-Rules/gpsg/#28-default-iterator-and-operator-rules)
+# ----------------------------------------------------------------------------------
+#
+# This example file contains pieces of code that either comply with or violate the
+# rule `do-not-use-has-key`.
