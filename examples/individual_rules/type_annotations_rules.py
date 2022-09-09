d6bbb2d19f7f73d49578da30229296fb6a515cec
Ruan Comelli <ruancomelli@gmail.com>
2022-09-09 15:36:56 -0300

    docs: add explanation for type annotation rules

---
+++ examples/individual_rules/type_annotations_rules.py
@@ -0,0 +1,6 @@
+# ----------------------------------------------------------------------------------
+# [2.21 Type Annotation Rules](https://docs.sourcery.ai/Reference/Custom-Rules/gpsg/#217-decorator-rules)
+# ----------------------------------------------------------------------------------
+#
+# This example file contains pieces of code that either comply with or violate the
+# rules `require-parameter-annotation` and `require-return-annotation`.
