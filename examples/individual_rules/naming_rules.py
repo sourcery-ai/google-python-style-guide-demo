892143f67f124ed43cdd15baa96a5a3b9042301f
Ruan Comelli <ruancomelli@gmail.com>
2022-09-09 15:54:48 -0300

    docs: add explanation for naming rules

---
+++ examples/individual_rules/naming_rules.py
@@ -0,0 +1,9 @@
+# ----------------------------------------------------------------------------------
+# [3.16 Naming Rules](https://docs.sourcery.ai/Reference/Custom-Rules/gpsg/#316-naming-rules)
+# ----------------------------------------------------------------------------------
+#
+# This example file contains pieces of code that either comply with or violate the
+# rules `avoid-single-character-names-variables`,
+# `avoid-single-character-names-functions`, `name-type-suffix`,
+# `snake-case-variable-declarations`, `snake-case-arguments`, `snake-case-functions`
+# and `upper-camel-case-classes`.
