98dfd32b36393294865416b1ec849ee1de35672a
Ruan Comelli <ruancomelli@gmail.com>
2022-09-15 09:26:53 -0300

    feat(examples/individual_rules/import_rules): add initial examples (#5)

--- examples/individual_rules/import_rules.py
+++ examples/individual_rules/import_rules.py
@@ -5,3 +5,34 @@
 # This example file contains pieces of code that either comply with or violate the
 # import rules `no-wildcard-imports` and `no-relative-imports`, as well as the
 # rules for standardized import aliases `use-standard-name-for-aliases-*`.
+
+# no violation
+import time
+
+# violates `no-wildcard-imports`
+from time import *
+
+# also violates `no-wildcard-imports`
+from rich.table import *
+
+# violate `no-relative-imports`
+from . import important_function
+from .my_module import important_function
+from ..my_module import important_function, unimportant_function
+
+# no violation - this is an absolute member import
+from my_company.my_module import important_function
+
+# violates both `no-wildcard-imports` and `no-relative-imports`
+from .something import *
+
+# no violation: import common libraries with their "standard" aliases
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+# violate `use-standard-name-for-aliases-*` rules
+# warning: chaos ahead!
+import numpy as pd
+import pandas as tf
+import tensorflow as np
