0584a713f77824e4bac2722a305aef4a132f7e8a
Ruan Comelli <ruancomelli@gmail.com>
2022-09-16 10:37:16 -0300

    fix(examples/individual_rules/import_rules): turn top-level comment into docstring

--- examples/individual_rules/import_rules.py
+++ examples/individual_rules/import_rules.py
@@ -1,10 +1,12 @@
-# ----------------------------------------------------------------------------------
-# [2.2 Import Rules](https://docs.sourcery.ai/Reference/Custom-Rules/gpsg/#22-import-rules)
-# ----------------------------------------------------------------------------------
-#
-# This example file contains pieces of code that either comply with or violate the
-# import rules `no-wildcard-imports` and `no-relative-imports`, as well as the
-# rules for standardized import aliases `use-standard-name-for-aliases-*`.
+"""
+----------------------------------------------------------------------------------
+[2.2 Import Rules](https://docs.sourcery.ai/Reference/Custom-Rules/gpsg/#22-import-rules)
+----------------------------------------------------------------------------------
+
+This example file contains pieces of code that either comply with or violate the
+import rules `no-wildcard-imports` and `no-relative-imports`, as well as the
+rules for standardized import aliases `use-standard-name-for-aliases-*`.
+"""
 
 # no violation
 import time
