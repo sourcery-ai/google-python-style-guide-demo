52ebd1b49fc3dde2822180812da04350f539f3b2
Ruan Comelli <ruancomelli@gmail.com>
2022-09-16 17:23:14 -0300

    feat(examples/individual_rules): add examples for getter and setter rules

--- examples/individual_rules/getter_and_setter_rules.py
+++ examples/individual_rules/getter_and_setter_rules.py
@@ -1,6 +1,71 @@
-# ----------------------------------------------------------------------------------
-# [3.15 Getter and Setter Rules](https://docs.sourcery.ai/Reference/Custom-Rules/gpsg/#315-getter-and-setter-rules)
-# ----------------------------------------------------------------------------------
-#
-# This example file contains pieces of code that either comply with or violate the
-# rule `avoid-trivial-properties`.
+"""
+----------------------------------------------------------------------------------
+[3.15 Getter and Setter Rules](https://docs.sourcery.ai/Reference/Custom-Rules/gpsg/#315-getter-and-setter-rules)
+----------------------------------------------------------------------------------
+
+This example file contains pieces of code that either comply with or violate the
+rule `avoid-trivial-properties`.
+"""
+
+# violates `avoid-trivial-properties` because the property `name` only reads from or
+# writes to an attribute without any extra logic
+class Student:
+    """A person that studies."""
+
+    @property
+    def name(self) -> str:
+        """Get the name of this student"""
+        return self._name
+
+    @name.setter
+    def name(self, new_name: str) -> None:
+        """Set the name of this student"""
+        self._name = new_name
+
+
+# no violation: the following class's `name` property is not trivial because it does not
+# contain an associated setter. You can use this pattern to "ensure" that an attribute
+# is read-only
+class Student:
+    """A person that studies."""
+
+    @property
+    def name(self) -> str:
+        """Get the name of this student"""
+        return self._name
+
+
+# no violation: the following class's `name` property is not trivial because the getter
+# method contains extra logic that is not simply returning an attribute
+class Student:
+    """A person that studies."""
+
+    @property
+    def name(self) -> str:
+        """Get the name of this student"""
+        return self._name.title()
+
+    @name.setter
+    def name(self, new_name: str) -> None:
+        """Set the name of this student"""
+        self._name = new_name
+
+
+# no violation: the following class's `name` property is not trivial because the setter
+# method contains extra logic that is not simply returning an attribute
+class Student:
+    """A person that studies."""
+
+    @property
+    def name(self) -> str:
+        """Get the name of this student"""
+        return self._name
+
+    @name.setter
+    def name(self, new_name: str) -> None:
+        """Set the name of this student"""
+        old_name = self._name
+        self._name = new_name
+
+        db = get_student_db()
+        db.replace(old_name, new_name)
